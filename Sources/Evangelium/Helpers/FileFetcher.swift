476c9e3614672d4891c10968bc07c236fd691d3e
guseducampos <guseducampos@gmail.com>
2019-10-01 11:41:39 -0600

    exit program when all of downloads finish.

---
+++ Sources/Evangelium/Helpers/FileFetcher.swift
@@ -0,0 +1,53 @@
+//
+//  File.swift
+//  
+//
+//  Created by Gustavo Campos on 10/1/19.
+//
+
+import Foundation
+
+
+class FileFetcher {
+    
+    private let dispatchGroup = DispatchGroup()
+    
+    var dateManager: DateManager
+    
+    let requester: ReadingRequester
+    
+    let fileManager: FileManager
+    
+    init(dateManager: DateManager,
+         requester: ReadingRequester,
+         fileManager: FileManager) {
+        self.dateManager = dateManager
+        self.requester = requester
+        self.fileManager = fileManager
+    }
+    
+    
+    func download(for language: Language, completion: @escaping () -> Void) {
+        DispatchQueue.concurrentPerform(iterations: 7) { _ in
+            let readingDate = try? dateManager.formattedDate()
+            print(readingDate ?? "")
+            dispatchGroup.enter()
+            requester.fecth(for: language.rawValue, in: readingDate ?? "") { result in
+                switch result {
+                case .success(let reading):
+                    fileManager.write(data: reading, languageFolder: language, filename: reading.date ?? "")
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    
+                }
+                self.dispatchGroup.leave()
+            }
+            dateManager.next()
+        }
+        
+        dispatchGroup.notify(queue: .main) { [weak self] in
+            self?.dateManager.reset()
+            completion()
+        }
+    }
+}
--- Sources/Evangelium/main.swift
+++ Sources/Evangelium/main.swift
@@ -1,25 +1,28 @@
 import Foundation
+import Dispatch
 
 let requester = ReadingRequester()
 let fileManager = FileManager()
 
-var dateManager = DateManager()
+let dateManager = DateManager()
 
-for language in Language.allCases {
-    for _ in 0 ..< 7 {
-        let readingDate = try dateManager.formattedDate()
-        
-        requester.fecth(for: language.rawValue, in: readingDate) { result in
-            switch result {
-            case .success(let response):
-                fileManager.write(data: response, languageFolder: language, filename: response.date ?? "")
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-        dateManager.next()
+let dispatchGroup = DispatchGroup()
+
+let languages =  Language.allCases
+
+DispatchQueue.concurrentPerform(iterations: languages.count) { index in
+    let fileFetcher = FileFetcher(dateManager: dateManager,
+                                  requester: requester,
+                                  fileManager: fileManager)
+    
+    dispatchGroup.enter()
+    fileFetcher.download(for: languages[index]) {
+        dispatchGroup.leave()
     }
-    dateManager.reset()
 }
 
-RunLoop.main.run(until: Date().addingTimeInterval(30))
+dispatchGroup.notify(queue: .main) {
+    exit(0)
+}
+
+RunLoop.main.run()
