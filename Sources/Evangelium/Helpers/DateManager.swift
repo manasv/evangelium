585a5a688e96453a8fe9d875f05e770ca22bb312
Manuel Sánchez <msanchez@applaudostudios.com>
2020-02-04 16:31:20 -0600

    :sparkles: Construct a date from an yyyy-MM-dd string

--- Sources/Evangelium/Helpers/DateManager.swift
+++ Sources/Evangelium/Helpers/DateManager.swift
@@ -39,3 +39,14 @@ extension DateManager {
         }
     }
 }
+
+// Construct a date from an yyyy-MM-dd Strings
+extension Date {
+    init(_ dateString:String) {
+        let dateStringFormatter = DateFormatter()
+        dateStringFormatter.dateFormat = "yyyy-MM-dd"
+        dateStringFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX") as Locale
+        let date = dateStringFormatter.date(from: dateString)!
+        self.init(timeInterval:0, since:date)
+    }
+}
